01159b1be963a57a21b1d80506e2682101954081
Sanoj-Omli <kumarisupriyasoni96@gmail.com>
2026-02-13 18:53:51 +0530

    bit manipulation

--- BitManipulation.cpp/ithBitSetOrNot.cpp
+++ BitManipulation.cpp/ithBitSetOrNot.cpp
@@ -1,24 +1,44 @@
 #include <iostream>
 using namespace std;
 
-bool checkKthBit(int n, int k)
-{
-    // check if k-th bit is set
-    // if(n &(1 << k )) != 0 // left shift
-    return (n >> k) & 1; // masking
+// Check if kth bit is set
+bool isKthBitSet(int n, int k) {
+    return (n & (1 << k)) != 0;
 }
-int main()
-{
-    int n = 5; // 0101
-    int k = 0;
 
-    if (checkKthBit(n, k))
-        cout << "Bit is set\n";
+// Set kth bit
+int setKthBit(int n, int k) {
+    return n | (1 << k);
+}
+
+// Clear kth bit
+int clearKthBit(int n, int k) {
+    return n & ~(1 << k);
+}
+
+// Toggle kth bit
+int toggleKthBit(int n, int k) {
+    return n ^ (1 << k);
+}
+
+int main() {
+    int n, k;
+
+    cout << "Enter number: ";
+    cin >> n;
+
+    cout << "Enter bit position (0-indexed): ";
+    cin >> k;
+
+    cout << "\nCheck kth bit: ";
+    if (isKthBitSet(n, k))
+        cout << "Bit is SET\n";
     else
-        cout << "Bit is not set\n";
+        cout << "Bit is NOT SET\n";
+
+    cout << "After setting kth bit:  " << setKthBit(n, k) << endl;
+    cout << "After clearing kth bit: " << clearKthBit(n, k) << endl;
+    cout << "After toggling kth bit: " << toggleKthBit(n, k) << endl;
 
     return 0;
 }
-
-// bool isKthBitSet(int n, int k) {return ((n >> k) & 1) == 1;} RightShift
-// bool isKthBitSet(int n, int k) {return (n & (1 << k)) != 0;} left shift
